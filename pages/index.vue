6af269e28c83208e87c6dc565133496481bdfb9c
Ben <benpaullamb@gmail.com>
2021-01-05 23:50:34 +0000

    chore: incremented version num

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
 		<div class="header">
 			<h1 class="header__title">
 				Training X
-				<span class="header__subtitle">v1.0.0</span>
+				<span class="header__subtitle">v1.1.0</span>
 			</h1>
 			<span class="material-icons" @click="copyProgramToClipboard"
 				>copy_all</span
